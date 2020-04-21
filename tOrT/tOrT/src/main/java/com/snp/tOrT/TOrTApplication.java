5ff4a6a7cedd6d2886b5cba4f85f7125b42a91b0
Paris-Downing <downingparis@yahoo.com>
2020-04-21 13:42:18 -0400

    added Paris-Downing

---
+++ tOrT/tOrT/src/main/java/com/snp/tOrT/TOrTApplication.java
@@ -0,0 +1,13 @@
+package com.snp.tOrT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TOrTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TOrTApplication.class, args);
+	}
+
+}
---
+++ tOrT/tOrT/src/test/java/com/snp/tOrT/TOrTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snp.tOrT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TOrTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
